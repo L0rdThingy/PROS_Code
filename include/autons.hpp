2633c54365eb4e04ac3f2fccc54397810a0b88e7
Josh Chu <91818392+L0rdThingy@users.noreply.github.com>
2021-12-04 11:38:16 -0800

    Add files via upload

---
+++ include/autons.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+void reset_constants();
+
+void left_tree_40();
+void right_tree_40();
+void left_lwp_29();
+void right_rwp_41();
+void solo_wp();
+void auto_6();
+void auto_7();
+void auto_8();
+void auto_9();
+void auto_10();
+
+void test_auton();
